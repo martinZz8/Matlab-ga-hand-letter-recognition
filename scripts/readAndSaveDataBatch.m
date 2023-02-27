2c8b190f0a88ceb38431515694daf7c0d5149ef7
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2023-02-27 17:31:01 +0100

    update of 'readAndSaveDataBatch.m' file

--- scripts/readAndSaveDataBatch.m
+++ scripts/readAndSaveDataBatch.m
@@ -1,10 +1,28 @@
-% Example invocation: readAndSaveDataBatch("archive/pattern_search/1", "results_1.xlsx", "maxIt", "funEv", [10,30,50,80,100,120], [200,500,1000,2000,2500],
-% @(X) (contains(X, "plM=GPSPositiveBasis2N") && contains(X, "plOA=Consecutive")),
-% @(X) readmatrix(X, 'Range', 'L18:L18', 'OutputType','double'),
-% @(X) writematrix(X, "archive/pattern_search/1/RESULTSX.xlsx", 'Range', 'C11')
-%);
-% readAndSaveDataBatch("archive/pattern_search/1", "results_1.xlsx", "maxIt", "funEv", [10,30,50,80,100,120], [200,500,1000,2000,2500], @(X) (contains(X, "plM=GPSPositiveBasis2N") && contains(X, "plOA=Consecutive")), @(X) readmatrix(X, 'Range', 'L18:L18', 'OutputType','double'), @(X) writematrix(X, "archive/pattern_search/1/RESULTSX.xlsx", 'Range', 'C11'));
-function [] = readAndSaveDataBatch(searchInDirName_, innerFileName_, rowItemName, columnItemName, rowItemsVector, columnItemsVector, containsFunc, readDataFunc, saveDataFunc)
+% Example invocation: readAndSaveDataBatch("GPSPositiveBasis2N", "Consecutive", "manhattan", true, 'C11'); OR readAndSaveDataBatch("GPSPositiveBasis2N", "Consecutive", "manhattan", false, 'K21');
+function [] = readAndSaveDataBatch(plM, plOA, metric, isEffectiveness, locationToSave)
+	% Data
+	searchInDirName_ = "archive/pattern_search/1";
+	innerFileName_ = "results_1.xlsx";
+	rowItemName = "maxIt";
+	columnItemName = "funEv";
+	rowItemsVector = [10,30,50,80,100,120];
+	columnItemsVector = [200,500,1000,2000,2500];
+	containsFunc = @(X) (contains(X, "plM="+plM) && contains(X, "plOA="+plOA) && contains(X, "metric="+metric));
+	function [dt] = readEffectiveness(filePath)
+		dt = readmatrix(filePath, 'Range', 'L18:L18', 'OutputType','double');
+    end
+	function [dt] = readTime(filePath)
+		mt = readmatrix(filePath, 'Range', 'A20:A20', 'OutputType','string');
+		sT = split(mt, ":");
+		sT = split(sT(3), "sec");
+		dt = str2double(strtrim(sT(1)));
+    end
+	readDataFunc = @(X) readEffectiveness(X);
+	if ~isEffectiveness
+		readDataFunc = @(X) readTime(X);
+    end
+	saveDataFunc = @(dt) writematrix(dt, searchInDirName_+"/RESULTS1.xlsx", 'AutoFitWidth', false, 'Range', locationToSave);
+	% Preparing result matrix
     resultData = zeros(length(rowItemsVector), length(columnItemsVector));
     % Read and process data
     d1 = dir(searchInDirName_);
@@ -28,8 +46,8 @@ function [] = readAndSaveDataBatch(searchInDirName_, innerFileName_, rowItemName
             d2 = dir(searchInDirName_+"/"+innerDirName);
             for j=1:length(d2)
                 innerFileName = d2(j).name;
-                if (innerFileName_ == innerFileName)
-                    disp(rowParam+ " " + colParam);
+                if (innerFileName_ == innerFileName && ~isnan(rowParam) && ~isnan(colParam))
+                    %disp(rowParam+ " " + colParam);
                     [~, rowIdx] = findFirstIndexInMatrix(rowItemsVector,rowParam);
                     [~, coldIdx] = findFirstIndexInMatrix(columnItemsVector,colParam);                   
                     fName = searchInDirName_+"/"+innerDirName+"/"+innerFileName;
@@ -42,5 +60,8 @@ function [] = readAndSaveDataBatch(searchInDirName_, innerFileName_, rowItemName
         end
     end
     % Save the data to specific matrix
+    disp("Result to save:");
     disp(resultData);
     saveDataFunc(resultData);
+    disp("Done");
+end
