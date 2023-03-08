dbe694ad86ba62c5bd7abc05dfca77261670a420
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2023-03-08 11:40:00 +0100

    up from particle-swarm

---
+++ fitnessFun2All.m
@@ -0,0 +1,17 @@
+% Uses euclidean function to establish dimension between two points (euclidean metric)
+function [J, transformedCloud, winningTemplateIndex] = fitnessFun2All(X, unknownCloud, templateClouds)
+[transformedCloud] = fitnessFunBase(X, unknownCloud);
+
+% exclude last point in last index (duplicated point on smallest finger)
+templatesCount = numel(templateClouds);
+Js = zeros(templatesCount, 1);
+for templateIndex=1:templatesCount
+    tempC = templateClouds{templateIndex}.Location(:,1:2);
+    transC = transformedCloud.Location(:,1:2);
+    diffsCloud = abs(tempC - transC);
+    for i=1:length(diffsCloud)
+        euclideanLength = sqrt(diffsCloud(i,1)^2 + diffsCloud(i,2)^2);
+        Js(templateIndex, 1) = Js(templateIndex, 1) + euclideanLength;
+    end
+end
+[J, winningTemplateIndex] = min(Js);
