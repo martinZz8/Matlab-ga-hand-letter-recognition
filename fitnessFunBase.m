d0a7b268c2441573588b2e4b03b78cf6529b4939
Maciej Harbuz <57568383+martinZz8@users.noreply.github.com>
2023-03-16 09:25:42 +0100

    up (like in sim-ann)

--- fitnessFunBase.m
+++ fitnessFunBase.m
@@ -4,8 +4,7 @@ function [transformedCloud] = fitnessFunBase(X, unknownCloud, isAngleInDegree)
 tform = rigidtform3d(eye(3,3), [X(1), X(2), 0]);
 translatedCloud = pctransform(unknownCloud, tform);
 % rotation (by axis Z)
-[cx, cy, cz] = getPointCloudCoG(translatedCloud);
-rotatedCloud = rotatePointCloud(translatedCloud, cx, cy, cz, 0, 0, X(3), isAngleInDegree);
+rotatedCloud = rotatePointCloud(translatedCloud, 0, 0, X(3), isAngleInDegree);
 % change of scale (on axis X and Y)
 tform = affinetform3d([X(4) 0 0 0; 0 X(5) 0 0; 0 0 1 0; 0 0 0 1]);
 transformedCloud = pctransform(rotatedCloud, tform);
